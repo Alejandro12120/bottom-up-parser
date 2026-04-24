093a93bae04f70f5d53b0861eee215dd0545ae25
Alejandro Rodríguez González <a.rodriguezgonzalez@stud.elka.pw.edu.pl>
2026-04-24 17:19:24 +0200

    forest builder implemented

---
+++ src/components/forest_builder.py
@@ -0,0 +1,90 @@
+from models.match import Match
+from models.node import Node
+from models.parser_state import ParserState
+from models.production import Production
+
+
+class ForestBuilder:
+    """
+    The ForestBuilder maintains the current partial parse forest throughout execution.
+    """
+
+    def __init__(self, state: ParserState):
+        self.__state = state
+
+    def apply_shift(self, symbol: str):
+        """A shift operation does::
+        - Create a terminal leaf node
+        - Append it into the forest
+        - Advance input position
+        """
+
+        # Keep in mind that the "forest" that the parser will use is the consumed
+        # The forest attribute is used for printing
+        # forest = consumed + remaining_input_as_terminal_nodes
+
+        self.__state.consumed.append(ForestBuilder.make_terminal_node(symbol))
+
+        if self.__state.remaining_input:
+            self.__state.remaining_input.pop(0)
+        self.__state.input_pos += 1
+
+        self.__state.build_full_forest()
+
+    def apply_reduction(self, match: Match):
+        """This method applies reduction, that involves:
+        - Get the nodes involved in the reduction
+        - Create a reduction node using the production
+        - Replace the nodes involved in the reduction by the reduction node
+        """
+
+        nodes_involved = self.__state.consumed[match.start_index: match.end_index]
+        reduced = ForestBuilder.make_reduction_node(match.production, nodes_involved)
+
+        self.__state.consumed = (
+                self.__state.consumed[:match.start_index] + [reduced] + self.__state.consumed[match.end_index:]
+        )
+
+        self.__state.right_most_derivation_hist.append(match.production.id)
+        self.__state.build_full_forest()
+
+    @staticmethod
+    def make_terminal_node(symbol: str) -> Node:
+        """This function creates a node for a terminal (a leaf) from a symbol
+
+        :param symbol: The nonterminal symbol
+        :returns: The leaf node
+        """
+        return Node(symbol=symbol, production_id=None, children=[])
+
+    @staticmethod
+    def make_reduction_node(production: Production, children: list[Node]) -> Node:
+        """Build and return a new reduction node.
+
+        This function is used when a reduction applies.
+
+        For example, if the forest looks like this::
+
+            [0] S/2
+                  └── a
+            [1] A/4
+                  └── b
+
+        And the reduction rule is:
+        S -> S A
+
+        Then this function returns the following node::
+
+            S/1
+              ├── S/2
+              │   └── a
+              └── A/4
+                  └── b
+
+        :param production: The production rule used for the reduction.
+        :param children: The nodes involved in the reduction.
+        :return: A node whose symbol is the production's left-hand side symbol and whose
+            children are the nodes involved in the reduction.
+        """
+        return Node(symbol=production.left_side, production_id=production.id,
+                    children=[child.clone() for child in children])
--- src/models/parser_state.py
+++ src/models/parser_state.py
@@ -1,15 +1,25 @@
 from dataclasses import dataclass
 
+from components.forest_builder import ForestBuilder
 from models.node import Node
 
 
 @dataclass(slots=True)
 class ParserState:
     input_pos: int
+    # Keep in mind that the "forest" that the parser will use is the consumed
+    # The forest attribute is used for printing
+    # forest = consumed + remaining_input_as_terminal_nodes
     forest: list[Node]
     consumed: list[Node]
     remaining_input: list[str]
     right_most_derivation_hist: list[int]
+
+    def build_full_forest(self):
+        """This method builds the total forest = consumed + remaining_input_as_terminal_nodes"""
+        self.forest = list(self.consumed)
+
+        self.forest.extend([ForestBuilder.make_terminal_node(symbol) for symbol in self.remaining_input])
 
     def clone(self) -> "ParserState":
         return ParserState(
